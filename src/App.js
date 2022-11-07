d88cc6d67ef7c4b2ca5d6eb9935c85c83d39f5c5
TheKnightKing <zadirab@gmail.com>
2022-11-07 11:12:32 +0300

    Post error

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ClassCounter from "./Components/ClassCounter";
+import Counter from "./Components/Counter";
+import PostItem from "./Components/PostItem";
+import PostList from "./Components/PostList";
+import MyButton from "./Components/UI/button/MyButton";
+import MyInput from "./Components/UI/input/MyInput";
+import "./style/App.css";
 
 function App() {
+  const [posts, usePosts] = useState([
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Javascript 2", body: "Description" },
+    { id: 3, title: "Javascript 3", body: "Description" },
+  ]);
+  const [title, setTitle] = useState('');
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <MyInput
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Название поста"
+        />
+        <MyInput type="text" placeholder="Опиасние поста" />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Посты про JS" />
     </div>
   );
 }
